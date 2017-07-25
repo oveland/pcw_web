82297a75897e9e4cb054565c64acbb63feb31f85
Oscar Velásquez <oiva.pcw@gmail.com>
2017-07-24 20:56:12 -0500

    Add new fields to ControlPointTimes for migration process

--- resources/views/migrations/_controlPoints.blade.php
+++ resources/views/migrations/_controlPoints.blade.php
@@ -18,20 +18,48 @@
                             </span>
                         </div>
                     </div>
-                    <div class="col-md-10">
+                    <div class="col-md-5">
                         <strong>{{ $controlPoint->name }}</strong>
                         <hr class="m-t-5 m-b-5">
                         <div class="m-t-2">
                             <div class="pull-left m-t-5">
-                                <i class="fa fa-flag-checkered text-purple" aria-hidden="true"></i> {{ $controlPoint->distance_from_dispatch }} m
+                                <span title="@lang('Distance from dispatch')"><i class="fa fa-flag-checkered text-purple" aria-hidden="true"></i> {{ $controlPoint->distance_from_dispatch }} m</span>
                                 <i class="fa fa-ellipsis-v m-r-10 m-l-10 text-muted" aria-hidden="true"></i>
-                                <i class="icon-direction text-info" aria-hidden="true"></i> {{ $controlPoint->distance_next_point }} m
+                                <span title="@lang('Distance to next point')"><i class="icon-direction text-info" aria-hidden="true"></i> {{ $controlPoint->distance_next_point }} m</span>
                             </div>
-                            <div class="pull-right m-t-3">
-                                <i class="fa fa-clock-o fa-spin text-danger" aria-hidden="true"></i>
-                                <span title="@lang('Time from dispatch') - @lang('Business day')" class="label label-inverse">{{ isset($controlPointTimes[0])?$controlPointTimes[0]->time_from_dispatch:'None' }}</span>
-                                <span class="label label-grey">{{ isset($controlPointTimes[0])?$controlPointTimes[1]->time_from_dispatch:'None' }}</span>
-                                <span class="label label-default">{{ isset($controlPointTimes[0])?$controlPointTimes[2]->time_from_dispatch:'None' }}</span>
+                        </div>
+                    </div>
+                    <div class="col-md-5 p-0">
+                        <div class="col-md-12">
+                            <div class="m-t-3">
+                                <i class="fa fa-calendar-check-o text-info" aria-hidden="true"></i>
+                                <span title="@lang('Time from dispatch') - @lang('Business day')" class="label label-inverse">
+                                    {{ isset($controlPointTimes[0])?$controlPointTimes[0]->time_from_dispatch:'None' }}
+                                </span>
+                                <i class="fa fa-clock-o fa-spin text-danger m-5" aria-hidden="true"></i>
+                                <span title="@lang('Time to next point') - @lang('Business day')" class="label label-inverse">
+                                    {{ isset($controlPointTimes[0])?$controlPointTimes[0]->time_next_point:'None' }}
+                                </span>
+                            </div>
+                            <div class="m-t-3">
+                                <i class="fa fa-calendar-check-o text-purple" aria-hidden="true"></i>
+                                <span title="@lang('Time from dispatch') - @lang('Saturday day')" class="label label-grey">
+                                    {{ isset($controlPointTimes[0])?$controlPointTimes[1]->time_from_dispatch:'None' }}
+                                </span>
+                                <i class="fa fa-clock-o fa-spin text-danger m-5" aria-hidden="true"></i>
+                                <span title="@lang('Time to next point') - @lang('Saturday day')" class="label label-grey">
+                                    {{ isset($controlPointTimes[0])?$controlPointTimes[1]->time_next_point:'None' }}
+                                </span>
+                            </div>
+                            <div class="m-t-3">
+                                <i class="fa fa-calendar-check-o text-warning" aria-hidden="true"></i>
+                                <span title="@lang('Time from dispatch') - @lang('Public holiday')" class="label label-default">
+                                    {{ isset($controlPointTimes[0])?$controlPointTimes[2]->time_from_dispatch:'None' }}
+                                </span>
+                                <i class="fa fa-clock-o fa-spin text-danger m-5" aria-hidden="true"></i>
+                                <span title="@lang('Time to next point') - @lang('Public holiday')" class="label label-default">
+                                    {{ isset($controlPointTimes[0])?$controlPointTimes[2]->time_next_point:'None' }}
+                                </span>
                             </div>
                         </div>
                     </div>
