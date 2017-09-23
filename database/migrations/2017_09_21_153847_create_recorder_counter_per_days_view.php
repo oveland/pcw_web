31571f6808acc992bf8caab281437bd92e324f72
Oscar Velásquez <oiva.pcw@gmail.com>
2017-09-23 11:29:02 -0500

    Update recorder_counter_per_days_view migration process

--- database/migrations/2017_09_21_153847_create_recorder_counter_per_days_view.php
+++ database/migrations/2017_09_21_153847_create_recorder_counter_per_days_view.php
@@ -14,20 +14,73 @@ class CreateRecorderCounterPerDaysView extends Migration
     public function up()
     {
         DB::statement("
-            CREATE OR REPLACE VIEW recorder_counter_per_days AS SELECT dr.date,
-                v.id AS vehicle_id,
-                v.company_id,
-                v.number,
-                max(dr.start_recorder) AS start_recorder,
-                CASE
-                    WHEN (max(dr.start_recorder) = 0) THEN (12)::bigint
-                    ELSE max(dr.start_recorder)
-                END AS start_recorder_prev,
-                max(dr.end_recorder) AS end_recorder,
-                (max(dr.end_recorder) - max(dr.start_recorder)) AS passengers
+            CREATE OR REPLACE VIEW recorder_counter_per_days AS 
+            SELECT dr.date,
+            v.id AS vehicle_id,
+            v.company_id,
+            v.number,
+            max(dr.start_recorder) AS start_recorder,
+            (
+            CASE
+            WHEN (max(dr.start_recorder) = 0) THEN
+              (
+                SELECT max(drp.end_recorder)
+                FROM dispatch_registers as drp
+                  WHERE drp.date < dr.date
+                  AND drp.vehicle_id = v.id
+                  AND drp.end_recorder > 0
+                GROUP BY drp.date
+                ORDER BY drp.date
+                DESC LIMIT 1
+              )
+            ELSE
+              max(dr.start_recorder)
+            END
+            ) AS start_recorder_prev,
+            
+            (
+            CASE
+            WHEN (max(dr.start_recorder) = 0) THEN
+              (
+                SELECT drp.date
+                FROM dispatch_registers as drp
+                WHERE drp.date < dr.date
+                      AND drp.vehicle_id = v.id
+                      AND drp.end_recorder > 0
+                GROUP BY drp.date
+                ORDER BY drp.date
+                DESC LIMIT 1
+              )
+            ELSE
+              max(dr.date)
+            END
+            ) AS date_start_recorder_prev,
+            
+            max(dr.end_recorder) AS end_recorder,
+            (
+            max(dr.end_recorder) -
+            (
+              CASE
+              WHEN (max(dr.start_recorder) = 0) THEN
+                (
+                  SELECT max(drp.end_recorder)
+                  FROM dispatch_registers as drp
+                  WHERE drp.date < dr.date
+                        AND drp.vehicle_id = v.id
+                        AND drp.end_recorder > 0
+                  GROUP BY drp.date
+                  ORDER BY drp.date
+                  DESC LIMIT 1
+                )
+              ELSE
+                max(dr.start_recorder)
+              END
+            )
+            ) AS passengers
             FROM (dispatch_registers dr
-                JOIN vehicles v ON ((v.id = dr.vehicle_id)))
-            WHERE (dr.status = 'Terminó' OR dr.status = 'En camino')
+            JOIN vehicles v ON ((v.id = dr.vehicle_id)))
+            WHERE (((dr.status)::text = 'Terminó'::text) OR ((dr.status)::text = 'En camino'::text))
+            --AND dr.date = '2017-09-18' AND v.number = '381'
             GROUP BY v.id, dr.date, v.company_id
         ");
     }
