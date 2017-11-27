6fb7e9ab21673f1831cd7a55d2f4db062d1a1146
Oscar Velásquez <oiva.pcw@gmail.com>
2017-11-26 22:13:50 -0500

    Minor changes

--- resources/views/template/sideBar.blade.php
+++ resources/views/template/sideBar.blade.php
@@ -74,7 +74,7 @@
                             <li class="has-sub menu-report-vehicles-parked">
                                 <a href="{{ route('report-vehicle-parked')  }}">
                                     <i class="fa fa-product-hunt" aria-hidden="true"></i>
-                                    @lang('Parking report')
+                                    @lang('Parked Report')
                                 </a>
                             </li>
                         </ul>
