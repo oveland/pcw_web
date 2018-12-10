1e8af9222cd55bd92aaba8b5957b8fcc26b11705
Ubuntu <ubuntu@ip-192-168-12-43>
2018-12-10 09:48:04 -0500

    Changes on server

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -52,7 +52,7 @@ class Kernel extends ConsoleKernel
         $schedule->command('gps:restart')->dailyAt('12:00');
         $schedule->command('gps:check-status')->everyMinute();
 
-        $schedule->command('send-mail:consolidated --prod=true')->dailyAt('04:00');
+        $schedule->command('send-mail:consolidated')->dailyAt('04:00');
         $schedule->command('send-mail:consolidated-passengers --prod=true')->dailyAt('04:10');
 
         $schedule->command('dar:run')->dailyAt('02:00');
