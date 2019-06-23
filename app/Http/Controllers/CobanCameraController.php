abcbb3cb659c065b9972c2b214dbfee58d522243
Ubuntu <ubuntu@ip-192-168-12-43>
2019-06-22 21:51:05 -0500

    MCS

--- app/Http/Controllers/CobanCameraController.php
+++ app/Http/Controllers/CobanCameraController.php
@@ -81,7 +81,7 @@ class CobanCameraController extends Controller
                 $location = $photo->location;
                 $report->push([
                     'id' => $photo->id,
-                    'date' => $photo->created_at->toDateTimeString(),
+                    'date' => ($report->count() + 1)." ⮞ ".$photo->date->toDateTimeString(),
                     'dispatchRegister' => $dispatchRegister ? $dispatchRegister->getAPIFields() : null,
                     'location' => $location ? $location->getAPIFields() : null
                 ]);
