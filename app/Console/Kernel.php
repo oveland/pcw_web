f0882361e97f6dcc0fc6ef32ac21a72bf7cc314c
Ubuntu <ubuntu@ip-192-168-12-43>
2020-03-09 10:22:59 -0500

    Ajustes en servidor para comandos de Rastreo de GPS

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -17,7 +17,8 @@ class Kernel extends ConsoleKernel
     {
         if (config('app.env') == 'local') {
             $schedule->command('track:map --company=21')->everyMinute()->between('04:00', '22:00');
-            $schedule->command('track:map --company=21')->everyFiveMinutes()->between('22:00', '04:00');
+            $schedule->command('track:map --company=21')->everyFiveMinutes()->between('22:00', '23:59');
+            $schedule->command('track:map --company=21')->everyFiveMinutes()->between('00:00', '04:00');
 
         } else {
             $schedule->command('log:parked-vehicles')->everyMinute();
