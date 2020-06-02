c2438c9fbccc4acd75e36b7be7a4738cdf8886b4
Ubuntu <ubuntu@ip-192-168-12-43>
2020-06-02 15:38:56 -0500

    Relación directa de vehiculos con app Rcket

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,7 +27,7 @@ class Kernel extends ConsoleKernel
 
             $schedule->command('telescope:prune')->daily();
 
-            $schedule->command('rocket:take-photo')->cron('*/5 * * * *')->between('05:00', '19:00');
+            $schedule->command('rocket:take-photo --vehicle-plate=VCK-542')->cron('*/5 * * * *')->between('05:00', '19:00');
         } else {
             $schedule->command('log:parked-vehicles')->everyMinute();
 
