bf3d8cd9972f6210aae0a7d7ee646cd9d5e12e3e
Ubuntu <ubuntu@ip-192-168-12-43>
2020-09-08 04:32:18 -0500

    MC server

--- app/Console/Commands/Tools/FixMileageCommand.php
+++ app/Console/Commands/Tools/FixMileageCommand.php
@@ -2,7 +2,9 @@
 
 namespace App\Console\Commands\Tools;
 
+use App\Models\Company\Company;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
 
 class FixMileageCommand extends Command
 {
@@ -39,31 +41,72 @@ class FixMileageCommand extends Command
     {
         $companyId = $this->option('company');
         $from = $this->option('from');
-        $maxMileage = 500000;
 
-        $lastLocations = \DB::select("
+        switch ($companyId) {
+            case Company::MONTEBELLO:
+                $maxMileage = 900000;
+                break;
+            case Company::YUMBENOS:
+                $maxMileage = 500000;
+                break;
+            default:
+                $maxMileage = 400000;
+                break;
+        }
+
+        $query = "
             SELECT id, date::DATE, vehicle_id, current_mileage, odometer, yesterday_odometer FROM last_locations
-            WHERE (odometer = 0 OR (current_mileage > $maxMileage OR current_mileage <= 0)) 
+            WHERE (current_mileage <= 0 or current_mileage > $maxMileage)
             AND date > '$from'
+            AND odometer <> yesterday_odometer
             AND vehicle_id IN (SELECT id FROM vehicles WHERE company_id = $companyId)
-limit 5
-        ");
+            limit 10000
+        ";
+
+        dump($query);
+
+        $lastLocations = \DB::select($query);
 
         foreach ($lastLocations as $lastLocation) {
-            $queryCM = "
-                select current_mileage
+            $this->info("- $lastLocation->id > $lastLocation->current_mileage m...");
+            $queryIO = "
+                select odometer
                 FROM locations
                 where date between ('$lastLocation->date 00:00:00')::timestamp without time zone and ('$lastLocation->date 23:59:59')::timestamp without time zone
                   and vehicle_id = $lastLocation->vehicle_id
-                  and current_mileage > 0
-                order by date DESC
+                  and odometer > 1000
+                order by date ASC
                 LIMIT 1;
             ";
+            $initialOdometer = collect(collect(\DB::select($queryIO))->first())->get('odometer');
 
-            $cm = collect(\DB::select($queryCM))->first();
+            $this->info("               initialOdometer = $initialOdometer");
 
-            dump($lastLocation, "---> ", $cm, "", "");
-//            $this->info("$lastLocation->id ");
+            if ($initialOdometer) {
+                $queryFO = "
+                    select odometer
+                    FROM locations
+                    where date between ('$lastLocation->date 00:00:00')::timestamp without time zone and ('$lastLocation->date 23:59:59')::timestamp without time zone
+                      and vehicle_id = $lastLocation->vehicle_id
+                      and odometer > 1000
+                    order by date DESC
+                    LIMIT 1;
+                ";
+                $finalOdometer = collect(collect(\DB::select($queryFO))->first())->get('odometer');
+
+                $this->info("               finalOdometer = $finalOdometer");
+
+                if ($finalOdometer) {
+                    $cm = intval($finalOdometer - $initialOdometer);
+
+                    $this->info("               diff = $cm");
+
+                    if ($cm > 0 && $cm < $maxMileage) {
+                        $this->info("   > Before $lastLocation->current_mileage Then $cm on id = $lastLocation->id ");
+                        \DB::statement("UPDATE last_locations SET current_mileage = $cm WHERE id = $lastLocation->id");
+                    }
+                }
+            }
         }
     }
 }
