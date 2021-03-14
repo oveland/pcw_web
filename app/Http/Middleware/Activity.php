c9a972ac1e7836d55f25db4999b2d43b45e0bb3b
Oscar Velasquez A <oiva.pcw@gmail.com>
2021-03-13 23:53:27 -0500

    Add activity HTTP request interceptor

---
+++ app/Http/Middleware/Activity.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class Activity
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param Request $request
+     * @param Closure $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+//        dd($request->segments());
+
+        return $next($request);
+    }
+}
