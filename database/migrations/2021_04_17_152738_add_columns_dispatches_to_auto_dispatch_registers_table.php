27250afc04c5f86cbf50055d49bf751b6231a853
Oscar Velasquez A <oiva.pcw@gmail.com>
2021-04-17 15:31:47 -0500

    Add columns dispatches (origin and destiniy) to auto dispatch registers table

---
+++ database/migrations/2021_04_17_152738_add_columns_dispatches_to_auto_dispatch_registers_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddColumnsDispatchesToAutoDispatchRegistersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('auto_dispatch_registers', function (Blueprint $table) {
+            $table->unsignedInteger('origin_dispatch_id')->nullable();
+            $table->unsignedInteger('destiny_dispatch_id')->nullable();
+
+            $table->foreign('origin_dispatch_id')->references('id')->on('dispatches')->onDelete('cascade');
+            $table->foreign('destiny_dispatch_id')->references('id')->on('dispatches')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('auto_dispatch_registers', function (Blueprint $table) {
+            $table->dropColumn('destiny_dispatch_id');
+            $table->dropColumn('origin_dispatch_id');
+        });
+    }
+}
