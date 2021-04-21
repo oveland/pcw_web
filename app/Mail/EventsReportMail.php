f248d385849f85f406fac0d4824827c4f61aad4d
Oscar Velasquez A <oiva.pcw@gmail.com>
2021-04-21 01:45:28 -0500

    Mc

--- app/Mail/EventsReportMail.php
+++ app/Mail/EventsReportMail.php
@@ -69,7 +69,7 @@ class EventsReportMail extends Mailable
     {
         $pathToConsolidatesReportFiles = $this->makeFiles();
 
-        $email = $this->view('email.reports.consolidated.daily')->subject(__('Route') . " | " . __('Report events daily') ." ".$this->company->name." ". " | $this->dateReport ".($this->production ? '* Revisado':'> Para revisar'));
+        $email = $this->view('email.reports.consolidated.daily')->subject(__('Route') . " • " . __('Report events daily') ." ".$this->company->name." ". " | $this->dateReport ".($this->production ? '':'> Para revisar'));
         if ($pathToConsolidatesReportFiles->isNotEmpty()) {
             foreach ($pathToConsolidatesReportFiles as $pathToConsolidatesReportFile) {
                 $email->attach($pathToConsolidatesReportFile);
