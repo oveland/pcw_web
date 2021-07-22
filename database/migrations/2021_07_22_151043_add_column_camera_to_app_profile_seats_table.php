681a6439fd01e90a938a0de5241c4eaa2e09634a
Oscar Velasquez <ovelandevs@gmail.com>
2021-07-22 15:14:19 -0500

    Se crea columna Camera para gestionar Multiples perfiles de topologias de asientos para Rocket

---
+++ database/migrations/2021_07_22_151043_add_column_camera_to_app_profile_seats_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddColumnCameraToAppProfileSeatsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('app_profile_seats', function (Blueprint $table) {
+            $table->dropUnique(['vehicle_id']);
+
+            $table->integer('camera')->nullable();
+
+            $table->unique(['vehicle_id', 'camera']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('app_profile_seats', function (Blueprint $table) {
+            $table->dropUnique(['vehicle_id', 'camera']);
+            $table->unique(['vehicle_id']);
+        });
+    }
+}
