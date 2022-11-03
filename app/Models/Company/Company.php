1bb77cf64fbd03d2b6f0751ec283a431b9bb4467
Olmer Velásquez <olmervelasquez@hotmail.com>
2022-11-03 12:43:29 -0500

    se agreaga mascara de camara 3 para VH 2225 de EP

--- app/Models/Company/Company.php
+++ app/Models/Company/Company.php
@@ -245,6 +245,16 @@ class Company extends Model
                 self::VALLEDUPAR
             ])->contains($this->id) || auth()->user()->isAdmin();
     }
+    function hasPhoto()
+    {
+        return collect([
+                self::YUMBENOS,
+                self::EXPRESO_PALMIRA,
+                self::MONTEBELLO,
+                self::TRANSPUBENZA,
+                self::VALLEDUPAR
+            ])->contains($this->id) || auth()->user()->isAdmin();
+    }
 
     /*
      * What companies that have seat sensor recorder counter
