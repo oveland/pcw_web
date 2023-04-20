4ee3145a1fd78a60739f8c174e3b9080f17c5895
Oscar Velasquez <ovelandevs@gmail.com>
2023-04-19 23:49:49 -0500

    :fire: Remove unnecesary files

--- .docker/config/configure.sh
+++ .docker/config/configure.sh
@@ -1,28 +1,37 @@
 #!/bin/sh
 # Configure PHP
-less $(echo "/etc/php/$PHP_VERSION/fpm/php.ini.template") | envsubst '\$PHP_TIMEZONE' > $(echo "/etc/php/$PHP_VERSION/fpm/php.ini")
-less $(echo "/etc/php/$PHP_VERSION/cli/php.ini.template") | envsubst '\$PHP_TIMEZONE' > $(echo "/etc/php/$PHP_VERSION/cli/php.ini")
+
+less $(echo "/etc/php/$PHP_VERSION/fpm/php.ini.template") | envsubst '\$PHP_TIMEZONE' >$(echo "/etc/php/$PHP_VERSION/fpm/php.ini")
+less $(echo "/etc/php/$PHP_VERSION/cli/php.ini.template") | envsubst '\$PHP_TIMEZONE' >$(echo "/etc/php/$PHP_VERSION/cli/php.ini")
 
 # Create php-fpm command
-printf '#!/bin/sh \n bash service php$PHP_VERSION-fpm start \n echo "    **** $PHP_VERSION-fpm runnning!" \n' > /usr/bin/php-fpm && chmod +x /usr/bin/php-fpm
+printf '#!/bin/sh \n bash service php$PHP_VERSION-fpm start \n echo "    **** $PHP_VERSION-fpm runnning!" \n' >/usr/bin/php-fpm && chmod +x /usr/bin/php-fpm
 
 # Configure nginx
-less /etc/nginx/conf.d/server.template | envsubst '\$NG_HOST \$NG_PORT \$NG_ROOT \$PHP_VERSION' > /etc/nginx/conf.d/server.conf
+less /etc/nginx/conf.d/server.template | envsubst '\$NG_HOST \$NG_PORT \$NG_ROOT \$PHP_VERSION' >/etc/nginx/conf.d/server.conf
 
-bash service nginx start;
+bash service nginx start
 echo "\n ****** Nginx server start on port: '$NG_PORT' | Exposed over external port: $NG_EXTERNAL_PORT"
 
-bash php-fpm;
+bash php-fpm
 
+cd "/var/www/$NG_ROOT"
+
+if [ -d "vendor" ]; then
+  echo "• Project $NG_ROOT ready"
+else
+  echo "Installing composer dependencies"
+  bash -c "composer install"
+  echo "• Project $NG_ROOT ready"
+fi
 
 #write out current crontab
-crontab -l > tmpcron
+crontab -l >tmpcron
 #echo new cron into cron file
-echo "* * * * * /usr/bin/php /var/www/$NG_ROOT/artisan schedule:run >> /dev/null 2>&1" >> tmpcron
+echo "* * * * * /usr/bin/php /var/www/$NG_ROOT/artisan schedule:run >> /dev/null 2>&1" >>tmpcron
 #install new cron file
 crontab tmpcron
 rm tmpcron
 
 bash service cron start
 echo "Cron configured successfully"
-
