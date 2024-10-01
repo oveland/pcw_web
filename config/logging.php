366c32059ceae1340614a7c08016f1adde3f5a07
ovelasquez <olmervelasquez@hotmail.com>
2024-10-01 14:06:26 -0500

    implementacion logging.php

---
+++ config/logging.php
@@ -0,0 +1,30 @@
+<?php
+
+return [
+
+    'default' => env('LOG_CHANNEL', 'stack'),
+
+    'channels' => [
+        'stack' => [
+            'driver' => 'stack',
+            'channels' => ['single'],
+            'ignore_exceptions' => false,
+        ],
+
+        'single' => [
+            'driver' => 'single',
+            'path' => storage_path('logs/laravel.log'),
+            'level' => 'debug',
+        ],
+
+        'daily' => [
+            'driver' => 'daily',
+            'path' => storage_path('logs/laravel.log'),
+            'level' => 'debug',
+            'days' => 14,
+        ],
+
+        // Puedes agregar más canales según tus necesidades
+    ],
+
+];
