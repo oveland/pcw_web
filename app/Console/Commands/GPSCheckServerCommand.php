e098956579403e343e9d794906ba122656de1a95
Olmer <olmervelasquez@hotmail.com>
2026-01-07 10:10:20 -0500

    Adjust server status alert logic in GPS check command

--- app/Console/Commands/GPSCheckServerCommand.php
+++ app/Console/Commands/GPSCheckServerCommand.php
@@ -55,7 +55,7 @@ class GPSCheckServerCommand extends Command
     public function handle()
     {
         $this->issues = collect([]);
-        $this->checkGPSServer();
+        //$this->checkGPSServer();
 //        $this->checkRecognitionServer();
 
         $this->checkDatabaseServer();
